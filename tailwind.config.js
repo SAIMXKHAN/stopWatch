9260f5c20d18566179b51348d4ad0f134505f22c
SAIMXKHAN <saim.tauseef27@gmail.com>
2025-01-07 17:21:29 -0800

    stopwatch

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          bebas: ['"Bebas Neue"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+  content: [
+    './path/to/your/html/files/*.html', // Make sure this path includes all your HTML files
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        stencil: ['"Big Shoulders Stencil Display"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+    
+  
