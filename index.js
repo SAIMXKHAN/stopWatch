51609c2f49b84a82b5f0e7e6a86dbea0881d848e
SAIMXKHAN <saim.tauseef27@gmail.com>
2025-01-07 18:51:24 -0800

    New Fix

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function reset(){
     startTime = 0;
     elapsedTime = 0;
     isRunning = false;    
-    display.textContent = "00:00:00";
+    display.textContent = "00:00:00:00";
 }
 
 function update(){
@@ -89,10 +89,12 @@ function update(){
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
     let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10)
 
     hours = String(hours).padStart(2, "0");
     minutes = String(minutes).padStart(2, "0");
     seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0")
 
-    display.textContent = `${hours}:${minutes}:${seconds}`;
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
